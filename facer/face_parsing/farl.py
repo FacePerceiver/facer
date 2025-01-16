c329ec38b45694270ad54f577be028626154f992
yinglinzheng <zhengyinglin@stu.xmu.edu.cn>
2025-01-16 20:20:39 +0800

    improve face parser for batch friendly

--- facer/face_parsing/farl.py
+++ facer/face_parsing/farl.py
@@ -7,6 +7,7 @@ from ..util import download_jit
 from ..transform import (get_crop_and_resize_matrix, get_face_align_matrix, get_face_align_matrix_celebm,
                          make_inverted_tanh_warp_grid, make_tanh_warp_grid)
 from .base import FaceParser
+import numpy as np
 
 pretrain_settings = {
     'lapa/448': {
@@ -58,8 +59,7 @@ class FaRLFaceParser(FaceParser):
     ```
     """
 
-    def __init__(self, conf_name: Optional[str] = None,
-                 model_path: Optional[str] = None, device=None) -> None:
+    def __init__(self, conf_name: Optional[str] = None, model_path: Optional[str] = None, device=None) -> None:
         super().__init__()
         if conf_name is None:
             conf_name = 'lapa/448'
@@ -68,25 +68,107 @@ class FaRLFaceParser(FaceParser):
         self.conf_name = conf_name
         self.net = download_jit(model_path, map_location=device)
         self.eval()
+        self.device = device
+        self.setting = pretrain_settings[conf_name]
+        self.label_names = self.setting['label_names']
 
-    def forward(self, images: torch.Tensor, data: Dict[str, Any]):
-        setting = pretrain_settings[self.conf_name]
-        images = images.float() / 255.0
+    
+    def get_warp_grid(self, images: torch.Tensor, matrix_src):
         _, _, h, w = images.shape
+        matrix = self.setting['get_matrix_fn'](matrix_src)
+        grid = self.setting['get_grid_fn'](matrix=matrix, orig_shape=(h, w))
+        inv_grid = self.setting['get_inv_grid_fn'](matrix=matrix, orig_shape=(h, w))
+        return grid, inv_grid
 
-        simages = images[data['image_ids']]
-        matrix = setting['get_matrix_fn'](data[setting['matrix_src_tag']])
-        grid = setting['get_grid_fn'](matrix=matrix, orig_shape=(h, w))
-        inv_grid = setting['get_inv_grid_fn'](matrix=matrix, orig_shape=(h, w))
+    def warp_images(self, images: torch.Tensor, data: Dict[str, Any]):
+        simages = self.unify_image_dtype(images)
+        simages = simages[data['image_ids']]
+        matrix_src = data[self.setting['matrix_src_tag']]
+        grid, inv_grid = self.get_warp_grid(simages, matrix_src)
 
         w_images = F.grid_sample(
             simages, grid, mode='bilinear', align_corners=False)
+        return w_images, grid, inv_grid
+    
 
-        w_seg_logits, _ = self.net(w_images)  # (b*n) x c x h x w
+    def decode_image_to_cv2(self, images: torch.Tensor):
+        '''
+        output: b x 3 x h x w, torch.uint8, [0, 255]
+        '''
+        assert images.ndim == 4
+        assert images.shape[1] == 3
+        images = images.permute(0, 2, 3, 1).cpu().numpy() * 255
+        images = images.astype(np.uint8)
+        return images
+
+    def unify_image_dtype(self, images: torch.Tensor|np.ndarray|list):
+        '''
+        output: b x 3 x h x w, torch.float32, [0, 1]
+        '''
+        if isinstance(images, np.ndarray):
+            images = torch.from_numpy(images)
+        elif isinstance(images, torch.Tensor):
+            pass
+        elif isinstance(images, list):
+            assert len(images) > 0, "images is empty"
+            first_image = images[0]
+            if isinstance(first_image, np.ndarray):
+                images = [torch.from_numpy(image).permute(2, 0, 1) for image in images]
+                images = torch.stack(images)
+            elif isinstance(first_image, torch.Tensor):
+                images = torch.stack(images)
+            else:
+                raise ValueError(f"Unsupported image type: {type(first_image)}")
+
+        else:
+            raise ValueError(f"Unsupported image type: {type(images)}")
+        
+        assert images.ndim == 4
+        assert images.shape[1] == 3
+
+        max_val = images.max()
+        if max_val <= 1:
+            assert images.dtype == torch.float32 or images.dtype == torch.float16
+        elif max_val <= 255:
+            assert images.dtype == torch.uint8
+            images = images.float() / 255.0
+        else:
+            raise ValueError(f"Unsupported image type: {images.dtype}")
+        if images.device != self.device:
+            images = images.to(device=self.device)
+        return images
+    
+    @torch.no_grad()
+    @torch.inference_mode()
+    def forward(self, images: torch.Tensor, data: Dict[str, Any]):
+        '''
+        images: b x 3 x h x w , torch.uint8, [0, 255]
+        data: {'rects': rects, 'points': points, 'scores': scores, 'image_ids': image_ids}
+        '''
+        w_images, grid, inv_grid = self.warp_images(images, data)
+        w_seg_logits = self.forward_warped(w_images, return_preds=False)
 
         seg_logits = F.grid_sample(
             w_seg_logits, inv_grid, mode='bilinear', align_corners=False)
 
-        data['seg'] = {'logits': seg_logits,
-                       'label_names': setting['label_names']}
+        data['seg'] = {'logits': seg_logits, 'label_names': self.label_names}
         return data
+    
+
+    def logits2predictions(self, logits: torch.Tensor):
+        return logits.argmax(dim=1)
+
+    @torch.no_grad()
+    @torch.inference_mode()
+    def forward_warped(self, images: torch.Tensor, return_preds: bool = True):
+        '''
+        images: b x 3 x h x w , torch.uint8, [0, 255]
+        '''
+        images = self.unify_image_dtype(images)
+        seg_logits, _ = self.net(images)  # nfaces x c x h x w
+        # seg_probs = seg_logits.softmax(dim=1)  # nfaces x nclasses x h x w
+        if return_preds:
+            seg_preds = self.logits2predictions(seg_logits)
+            return seg_logits, seg_preds, self.label_names
+        else:
+            return seg_logits
